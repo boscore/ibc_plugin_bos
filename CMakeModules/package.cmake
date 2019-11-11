e563c7e108541f6636d62057f1e996e5977973dd
Henrik Hautakoski <henrik@eossweden.org>
2019-11-11 13:25:13 +0100

    CMakeModules/package.cmake: Update information.

--- CMakeModules/package.cmake
+++ CMakeModules/package.cmake
@@ -1,8 +1,8 @@
-set(VENDOR "block.one")
-set(PROJECT_NAME "eosio")
-set(DESC "Software for the EOS.IO network")
-set(URL "https://github.com/eosio/eos")
-set(EMAIL "support@block.one")
+set(VENDOR "BOS Core")
+set(PROJECT_NAME "bos")
+set(DESC "Software for the BOS network")
+set(URL "https://github.com/boscore/bos")
+set(EMAIL "support@boscore.io")
 
 configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_package.sh.in ${CMAKE_BINARY_DIR}/packages/generate_package.sh @ONLY)
 configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_bottle.sh ${CMAKE_BINARY_DIR}/packages/generate_bottle.sh COPYONLY)
